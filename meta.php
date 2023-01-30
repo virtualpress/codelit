50e087305af011e55ae4d18c024cd0ae3e18ce44
mwektaehtabr <mwektaehtabr@gmail.com>
2023-01-30 18:52:31 +0100

    //

--- meta.php
+++ meta.php
@@ -1,20 +1,48 @@
-<meta name="description" content="<?php
-  if (date("a") == "pm") {
-   echo "cyber zine";
-  } else {
-   echo "not not malware";
-  }
-?>">
+<meta name="description" content="cyber zine">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
   <meta property="og:url" content="https://www.codelit.com" />
   <meta property="og:title" content="CODE LIT" />
   <meta property="og:description" content="cyber zine" />
-  <meta property="og:video" content="https://www.codelit.com/codelit.mp4" />
+  <meta property="og:video" content="https://www.codelit.com/<?php
+  switch (rand(0,3)) {
+   case 0:
+    echo "root";
+    break;
+   case 1:
+    echo "bash";
+    break;
+   case 2:
+    echo "zsh";
+    break;
+   case 3:
+    echo "msdos";
+    break;      
+   default:
+    echo "root";
+  }
+?>.mp4" />
   <meta property="og:video:type" content="video/mp4" />
   <meta property="og:video:width" content="2636" />
   <meta property="og:video:height" content="1380" />
-  <meta property="og:image" content="https://www.codelit.com/cyberzine.jpg" />
+  <meta property="og:image" content="https://www.codelit.com/<?php
+  switch (rand(0,3)) {
+   case 0:
+    echo "root";
+    break;
+   case 1:
+    echo "bash";
+    break;
+   case 2:
+    echo "zsh";
+    break;
+   case 3:
+    echo "msdos";
+    break;      
+   default:
+    echo "root";
+  }
+?>.jpg" />
   <meta name="twitter:card" content="summary" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
