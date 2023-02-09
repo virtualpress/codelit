50142144f56ddaf8826d0b9f21551049fb53c9f2
mwektaehtabr <mwektaehtabr@gmail.com>
2023-02-09 19:52:23 +0100

    //

--- frame-of-reference.js
+++ frame-of-reference.js
@@ -1,9 +1,9 @@
-for (var n = 0; n < document.body.getElementsByClassName("lit").length - 1; n++) {
+for (var n = 0; n < document.body.getElementsByClassName("lit").length; n++) {
  document.body.getElementsByClassName("lit")[n].addEventListener("click", lit);
 }
 
 function lit() {
- for (var n = 0; n < document.body.getElementsByClassName("lit").length - 1; n++) {
+ for (var n = 0; n < document.body.getElementsByClassName("lit").length; n++) {
   transparent(document.body.getElementsByClassName("lit")[n]);
  }
  opaque(event.target);
