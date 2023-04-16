be38586d91b06de272b19fb89291cab45645a6c8
mwektaehtabr <mwektaehtabr@gmail.com>
2023-04-16 12:20:42 +0900

    //

--- LICENSE.md
+++ LICENSE.md
@@ -1,3 +1,3 @@
 # LICENSE
 
-This code is distributed under a [Creative Commons Attribution 4.0 International license](https://creativecommons.org/licenses/by/4.0/).
+This code is distributed under a [Creative Commons CC0 1.0 Universal Public Domain Dedication](https://creativecommons.org/publicdomain/zero/1.0/).
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  </head>
  <body>
   <?php include "header.php";?>
+   <p class="contents"><a href="brain-map" class="title">BRAIN MAP</a><br>// <a href="https://www.annevalente.com/" target="_blank" class="author">anne valente</a></p>
    <p class="contents"><a href="obit-in-translation" class="title">OBIT, IN TRANSLATION</a><br>// <a href="https://liwenxu.wixsite.com/website" target="_blank" class="author">liwen xu</a></p>
    <p class="contents"><a href="poem-in-which-foreboding-again-figures-prominently" class="title">POEM IN WHICH FOREBODING AGAIN FIGURES PROMINENTLY</a><br>// <a href="https://katemlucas.com/" target="_blank" class="author">kate lucas</a></p>
    <p class="contents"><a href="inspector-4" class="title">INSPECTOR 4</a><br>// <a href="https://www.fourforaquarter.com/" target="_blank" class="author">michael martone</a></p>
