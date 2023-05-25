cd2f6abb8194a1195be5d4a60c04c03e7046fc77
mwektaehtabr <mwektaehtabr@gmail.com>
2023-05-25 11:54:21 -0400

    //

--- excoriator.php
+++ excoriator.php
@@ -2,13 +2,13 @@
 <?php include "comment.php";?>
 <html>
  <head>
-  <title>EXCORIATOR // sam marshall</title>
+  <title>EXCORIATOR // nick marshall</title>
   <?php include "meta.php";?>
   <link rel="stylesheet" type="text/css" href="excoriator.css">
  </head>
  <body>
   <?php include "header.php";?>
-   <p class="contents">EXCORIATOR<br>// sam marshall</p>
+   <p class="contents">EXCORIATOR<br>// nick marshall</p>
    <div class="content">
     <p class="excoriator">EXCORIATOR</p><p>The Scab calls on a Thursday, says he has something to show <span class="lit">you</span>. When he says that he has not seen or spoken to anyone in weeks, you believe him. He tells you not to ask questions &mdash; just <span class="lit">drop everything and go</span>.</p>
    </div>
