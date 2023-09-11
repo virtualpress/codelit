b661c948e2e7baa3e39edc6da1ab4b5f536aad07
mwektaehtabr <mwektaehtabr@gmail.com>
2023-09-11 00:10:45 -0400

    //

--- meta.php
+++ meta.php
@@ -1,10 +1,10 @@
 <meta name="description" content="cyber zine">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
-  <meta property="og:url" content="https://www.codelit.com" />
+  <meta property="og:url" content="https://codelit.com" />
   <meta property="og:title" content="CODE LIT" />
   <meta property="og:description" content="cyber zine" />
-  <meta property="og:video" content="https://www.codelit.com/<?php
+  <meta property="og:video" content="https://codelit.com/<?php
   switch (rand(0,3)) {
    case 0:
     echo "root";
@@ -25,7 +25,7 @@
   <meta property="og:video:type" content="video/mp4" />
   <meta property="og:video:width" content="2636" />
   <meta property="og:video:height" content="1380" />
-  <meta property="og:image" content="https://www.codelit.com/<?php
+  <meta property="og:image" content="https://codelit.com/<?php
   switch (rand(0,3)) {
    case 0:
     echo "root";
