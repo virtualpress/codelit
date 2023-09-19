faa09fa657979757ee92499c07145070ecbf0060
mwektaehtabr <mwektaehtabr@gmail.com>
2023-09-19 01:30:14 -0400

    //

---
+++ another-thrill.js
@@ -0,0 +1,78 @@
+var right = ["all", "and", "asked", "bottle", "built-up", "buy", "can", "cash", "cold", "dandruff", "expensive", "falls", "finding", "first", "for", "golden", "grease", "hair", "half", "in", "into", "life", "little", "locks", "month", "most", "next", "night", "no", "of", "punishing", "questions", "return", "right", "shampoo", "shines", "shop", "the", "then", "think", "thrill", "time", "to", "trying", "until", "walking", "word", "you", "your"];
+
+var wrong = ["a", "about", "adventure", "after", "afternoon", "also", "amusement", "an", "answers", "any", "apricot", "as", "ask", "at", "back", "be", "because", "before", "bergamot", "blond", "blonde", "blood", "bonanza", "borrow", "but", "by", "cardamom", "cedar", "champagne", "chance", "cheap", "choice", "choose", "cinnamon", "citrus", "clay", "coconut", "coin", "cologne", "come", "con", "conditioner", "contentment", "copper", "could", "cucumber", "day", "days", "decide", "delight", "desire", "destiny", "digging", "dirt", "dirty", "discovering", "discovery", "do", "dollars", "down", "drifting", "dripping", "dumpster", "dumpster-diving", "dust", "dusty", "entire", "even", "evening", "excitement", "experience", "fancy", "fate", "floral", "flowery", "foam", "foaming", "foamy", "fog", "fogging", "foggy", "fortune", "frankincense", "free", "freedom", "from", "garbage", "gem", "gems", "get", "getaway", "give", "glory", "glow", "glows", "go", "gold", "gold-mining", "good", "goods", "grapefruit", "grime", "hack", "happiness", "hard", "have", "heat", "hot", "how", "hunger", "idea", "if", "imagine", "instant", "it", "its", "jackpot", "jar", "jasmine", "jewel", "jewels", "joy", "jug", "just", "know", "large", "last", "later", "lavender", "left", "lemon", "liberty", "like", "lilac", "lime", "look", "looking", "loophole", "loot", "luck", "luxe", "luxury", "make", "mango", "market", "melon", "memory", "might", "mist", "misting", "misty", "moment", "money", "months", "morning", "much", "mud", "muddy", "nectarine", "new", "none", "not", "now", "off", "oil", "on", "one", "only", "onto", "or", "orange", "other", "out", "over", "paragraph", "passionfruit", "peach", "perfume", "pineapple", "pleasure", "plum", "plunder", "poem", "poetry", "posh", "premium", "prize", "puzzling", "rich", "riches", "rose", "rosemary", "rust", "sage", "said", "sandalwood", "satisfaction", "say", "scalp", "scavenge", "scavenged", "scent", "scouring", "scrubbing", "searching", "second", "see", "sell", "sensation", "sentence", "shimmer", "shimmers", "shine", "short", "silver", "skin", "smell", "so", "soap", "soft", "some", "soon", "sound", "spills", "splashing", "steal", "steam", "steaming", "steamy", "store", "story", "strands", "streaming", "strolling", "swank", "sweat", "sweaty", "take", "tangerine", "tell", "than", "that", "their", "them", "there", "these", "they", "thin", "this", "told", "towel", "trash", "treasure", "treasure-hunting", "trickling", "tumbles", "twilight", "under", "urge", "use", "vanilla", "wandering", "want", "warm", "water", "way", "wealth", "week", "weeks", "well", "what", "when", "where", "which", "who", "whole", "will", "with", "words", "work", "would", "wrong", "year", "years", "yellow", "yes", "yet", "yuzu"];
+
+n = 0;
+
+var t;
+
+if (touch) {
+ t = 24;
+} else {
+ t = 48;
+}
+
+while (n < t) {
+ var i = random(right.length);
+ while (right[i] == undefined) {
+  i = random(right.length);
+ }
+ width = random(80);
+ height = random(76) + 4;
+ if (touch) {
+  document.getElementById("words").innerHTML += "<span class=\"right\" style=\"left:" + width + "vw; top:" + height + "vh;\" onclick=\"thrill()\">" + right[i] + "</span>";
+ } else {
+  document.getElementById("words").innerHTML += "<span class=\"right\" style=\"left:" + width + "vw; top:" + height + "vh;\" onmouseover=\"thrill()\">" + right[i] + "</span>";
+ }
+ delete right[i];
+ n++;
+}
+
+if (touch) {
+ t = 96;
+} else {
+ t = 192;
+}
+
+while (n < t) {
+ var i = random(wrong.length);
+ while (wrong[i] == undefined) {
+  i = random(wrong.length);
+ }
+ width = random(80);
+ height = random(76) + 4;
+ if (touch) {
+  document.getElementById("words").innerHTML += "<span class=\"wrong\" style=\"left:" + width + "vw; top:" + height + "vh;\" onclick=\"thrill()\">" + wrong[i] + "</span>";
+ } else {
+  document.getElementById("words").innerHTML += "<span class=\"wrong\" style=\"left:" + width + "vw; top:" + height + "vh;\" onmouseover=\"thrill()\">" + wrong[i] + "</span>";
+ }
+ delete wrong[i];
+ n++;
+}
+
+var v = 0;
+
+if (touch) {
+ t = 11;
+} else {
+ t = 23;
+}
+
+function thrill() {
+ none(event.target);
+ if (event.target.className == "right") {
+  var s = 0;
+  while (s < document.body.querySelectorAll(".lit").length) {
+   if (event.target.innerText == document.body.querySelectorAll(".lit")[s].innerHTML.toLowerCase()) {
+    visible(document.body.querySelectorAll(".lit")[s]);
+   }
+   s++;
+  }
+  v++;
+  if (v > t) {
+   document.body.querySelectorAll("#words").forEach(none);
+   document.body.querySelectorAll(".lit").forEach(visible);
+   document.body.querySelectorAll(".lit").forEach(opaque);
+  }
+ }
+}
---
+++ another-thrill.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php include "comment.php";?>
+<html>
+ <head>
+  <title>ANOTHER THRILL // anders carlson-wee</title>
+  <?php include "meta.php";?>
+  <link rel="stylesheet" type="text/css" href="another-thrill.css">
+ </head>
+ <body>
+  <?php include "header.php";?>
+   <p class="contents">ANOTHER THRILL<br>// <a href="https://www.anderscarlsonwee.com" target="_blank" class="author">anders carlson-wee</a></p>
+   <div class="content">
+    <p id="words"></p>
+    <p><span class="lit">Half</span> <span class="lit">the</span> <span class="lit">night</span> <span class="lit">trying</span> <span class="lit">to</span> <span class="lit">think</span></p>
+    <p><span class="lit">of</span> <span class="lit">the</span> <span class="lit">right</span> <span class="lit">word</span> <span class="lit">for</span> <span class="lit">the</span> <span class="lit">thrill</span></p>
+    <p><span class="lit">of</span> <span class="lit">finding</span> <span class="lit">the</span> <span class="lit">most</span> <span class="lit">expensive</span></p>
+    <p><span class="lit">shampoo</span> <span class="lit">you</span> <span class="lit">can</span> <span class="lit">buy</span> <span class="lit">and</span> <span class="lit">all</span></p>
+    <p><span class="lit">the</span> <span class="lit">next</span> <span class="lit">month</span> <span class="lit">punishing</span></p>
+    <p><span class="lit">dandruff</span> <span class="lit">and</span> <span class="lit">built-up</span> <span class="lit">grease</span></p>
+    <p><span class="lit">until</span> <span class="lit">your</span> <span class="lit">hair</span> <span class="lit">shines</span> <span class="lit">and</span> <span class="lit">falls</span></p>
+    <p><span class="lit">in</span> <span class="lit">little</span> <span class="lit">golden</span> <span class="lit">locks</span> <span class="lit">and</span> <span class="lit">then</span></p>
+    <p><span class="lit">walking</span> <span class="lit">into</span> <span class="lit">the</span> <span class="lit">shop</span></p>
+    <p><span class="lit">for</span> <span class="lit">the</span> <span class="lit">first</span> <span class="lit">time</span> <span class="lit">in</span> <span class="lit">your</span> <span class="lit">life</span></p>
+    <p><span class="lit">to</span> <span class="lit">return</span> <span class="lit">the</span> <span class="lit">bottle</span> <span class="lit">for</span> <span class="lit">cold</span></p>
+    <p><span class="lit">cash</span> <span class="lit">no</span> <span class="lit">questions</span> <span class="lit">asked.</span></p>
+   </div>
+  <?php include "footer.php";?>
+  <script src="another-thrill.js"></script>
+ </body>
+</html>
