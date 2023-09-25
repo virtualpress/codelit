c2985a107e558c9593a45625ef3515a4744e9f31
mwektaehtabr <mwektaehtabr@gmail.com>
2023-09-25 09:27:57 -0400

    //

--- index.php
+++ index.php
@@ -7,8 +7,104 @@
  </head>
  <body>
   <?php include "header.php";?>
+   <?php
+    $contents = array(
+     array("page"=>"between-the-lines", "title"=>"BETWEEN THE LINES", "link"=>"https://www.kellyluce.com/", "author"=>"kelly luce"),
+     array("page"=>"meditations-on-the-line", "title"=>"MEDITATIONS ON THE LINE", "link"=>"https://www.tianaclark.com/", "author"=>"tiana clark"),
+     array("page"=>"obit-in-translation", "title"=>"OBIT, IN TRANSLATION", "link"=>"https://liwenxu.wixsite.com/website", "author"=>"liwen xu"),
+     array("page"=>"discrepancies", "title"=>"DISCREPANCIES", "link"=>"https://mwektaehtabr.com/", "author"=>"matthew baker"),
+     array("page"=>"three-men-in-trouble", "title"=>"THREE MEN IN TROUBLE", "link"=>"https://lydia-conklin.com/", "author"=>"lydia conklin"),
+     array("page"=>"romance-languages", "title"=>"ROMANCE LANGUAGES", "link"=>"https://kanakkapur.carrd.co/", "author"=>"kanak kapur"),
+     array("page"=>"criminality", "title"=>"CRIMINALITY", "link"=>"https://umkalra.carrd.co", "author"=>"umang kalra"),
+     array("page"=>"excoriator", "title"=>"EXCORIATOR", "link"=>"", "author"=>"nick marshall"),
+     array("page"=>"brain-map", "title"=>"BRAIN MAP", "link"=>"https://www.annevalente.com/", "author"=>"anne valente"),
+     array("page"=>"poem-in-which-foreboding-again-figures-prominently", "title"=>"POEM IN WHICH FOREBODING AGAIN FIGURES PROMINENTLY", "link"=>"https://katemlucas.com/", "author"=>"kate lucas"),
+     array("page"=>"how-to-write-a-novel-in-seven-years", "title"=>"HOW TO WRITE A NOVEL IN SEVEN YEARS", "link"=>"https://www.jmarceloborromeo.com/", "author"=>"j marcelo borromeo"),
+     array("page"=>"ephemeral", "title"=>"EPHEMERAL", "link"=>"https://mwektaehtabr.com/", "author"=>"matthew baker"),
+     array("page"=>"terminal", "title"=>"TERMINAL", "link"=>"https://www.keithrleonard.com/", "author"=>"keith leonard"),
+     array("page"=>"eat-me", "title"=>"EAT ME (ALTERNATIVE SCENARIOS IN THE GARDEN OF EDEN)", "link"=>"https://www.allegrahyde.com/", "author"=>"allegra hyde"),
+     array("page"=>"frame-of-reference", "title"=>"FRAME OF REFERENCE", "link"=>"https://www.simonhan.com/", "author"=>"simon han"),
+     array("page"=>"who-you-are-who-am-i", "title"=>"WHO YOU ARE, WHO AM I", "link"=>"https://www.autumnhouse.org/our-authors/cundieff-melissa/", "author"=>"melissa cundieff"),
+     array("page"=>"another-thrill", "title"=>"ANOTHER THRILL", "link"=>"https://www.anderscarlsonwee.com", "author"=>"anders carlson-wee"),
+     array("page"=>"therapy-worksheet-cognitive-distortion-analysis", "title"=>"THERAPY WORKSHEET: COGNITIVE DISTORTION ANALYSIS", "link"=>"https://www.rachelmennies.com/", "author"=>"rachel mennies"),
+     array("page"=>"a-permanent-but-shifting-valley", "title"=>"A PERMANENT BUT SHIFTING VALLEY", "link"=>"https://sam-ross-4cah.squarespace.com/", "author"=>"sam ross"),
+      array("page"=>"reverberations", "title"=>"REVERBERATIONS", "link"=>"https://sam-ross-4cah.squarespace.com/", "author"=>"sam ross"),
+     array("page"=>"the-extinction-of-homo-sapiens", "title"=>"THE EXTINCTION OF HOMO SAPIENS", "link"=>"https://mwektaehtabr.com/", "author"=>"matthew baker"),
+     array("page"=>"inspector-4", "title"=>"INSPECTOR 4", "link"=>"https://www.fourforaquarter.com/", "author"=>"michael martone"),
+     array("page"=>"exchange", "title"=>"EXCHANGE", "link"=>"https://www.rosaliemoffett.com/", "author"=>"rosalie moffett"),
+     array("page"=>"mirror-walkers", "title"=>"MIRROR WALKERS", "link"=>"https://www.naheedphirozepatel.com/", "author"=>"naheed phiroze patel"),
+     array("page"=>"once", "title"=>"ONCE", "link"=>"https://www.chadabushanab.com/", "author"=>"chad abushanab"),
+    );
+    while (sizeof($contents) > 0) {
+     if (rand(0,7) == 0) {
+      $n = 0;
+     } else {
+      $n = rand(0, sizeof($contents) - 1);
+     }
+     if (sizeof($contents) > 16 && $n >= sizeof($contents) - 3) {
+      $n = rand(0,7);
+     }
+     if ($contents[$n]["link"] != "") {
+      echo '<p class="contents"><a href="';
+      echo $contents[$n]["page"];
+      echo '" class="title">';
+      echo $contents[$n]["title"];
+      echo '</a><br>// <a href="';
+      echo $contents[$n]["link"];
+      echo '" target="_blank" class="author">';
+      echo $contents[$n]["author"];
+      echo '</a></p>';
+     } else {
+      echo '<p class="contents"><a href="';
+      echo $contents[$n]["page"];
+      echo '" class="title">';
+      echo $contents[$n]["title"];
+      echo '</a><br>// ';
+      echo $contents[$n]["author"];
+      echo '</p>';
+     }
+     array_splice($contents, $n, 1);
+    }
+   ?>
+   <p class="contents"><a href="writers-block" class="title">WRITER'S BLOCK</a><br>// <a href="https://mwektaehtabr.com" target="_blank" class="author">matthew baker</a></p>
+   <p id="footer"><?php
+    $n = rand(0,99);
+    if ($n < 64) {
+     echo '<a href="about">ABOUT</a>';
+    } elseif ($n < 80) {
+     echo '<a onclick="erase();">DELETE WEBSITE</a>';
+    } elseif ($n < 84) {
+     echo '<a onclick="read();">READ SOMETHING RANDOM</a>';
+    } elseif ($n < 86) {
+     echo '<a href="mailto:codelit@codelit.com">CONTACT</a>';
+    } elseif ($n < 88) {
+     echo '<a href="mailto:codelit@codelit.com">SUBMIT</a>';
+    } elseif ($n < 90) {
+     echo '<a href="https://defcon.org" target="_blank">GO TO DEF CON</a>';
+    } elseif ($n == 90) {
+     echo '<a href="https://supporters.eff.org/donate/join-eff-4" target="_blank">DONATE TO THE ELECTRONIC FRONTIER FOUNDATION</a>';
+    } elseif ($n == 91) {
+     echo '<a href="https://www.ddosecrets.com/wiki/Contribute" target="_blank">DONATE TO DDOSECRETS</a>';
+    } elseif ($n == 92) {
+     echo '<a href="https://shop.wikileaks.org/donate" target="_blank">DONATE TO WIKILEAKS</a>';
+    } elseif ($n == 93) {
+     echo '<a href="https://www.ddosecrets.com/wiki/Contact" target="_blank">SEND A CLASSIFIED DOC TO DDOSECRETS</a>';
+    } elseif ($n == 94) {
+     echo '<a href="https://wikileaks.com/#submit" target="_blank">SEND A CLASSIFIED DOC TO WIKILEAKS</a>';
+    } elseif ($n == 95) {
+     echo '<a href="mailto:potus@whitehouse.gov">EMAIL THE PRESIDENT</a>';
+    } elseif ($n == 96) {
+     echo '<a href="http://en.letters.kremlin.ru/letters/send" target="_blank">FILE A LETTER OF COMPLAINT WITH THE KREMLIN</a>';
+    } elseif ($n == 97) {
+     echo '<a href="mailto:timbl@w3.org">SEND FAN MAIL TO THE CREATOR OF THE WORLD WIDE WEB</a>';
+    } elseif ($n == 98) {
+     echo '<a href="https://github.com/virtualpress/codelit" target="_blank">VIEW SOURCE CODE</a>';
+    } else {
+     echo '<a href="https://en.wikipedia.org/wiki/Special:Random">READ SOMETHING RANDOM ON WIKIPEDIA INSTEAD</a>';
+    }
+   ?></p>
   <?php include "footer.php";?>
   <div id="egg"></div>
-  <script src="localhost.js"></script>
+  <script src="sudo.js"></script>
  </body>
 </html>
---
+++ sudo.js
@@ -0,0 +1,156 @@
+function namer() {
+ var n = random(100);
+ if (n < 64) {
+  return "code lit";
+ } else if (n < 80) {
+  return "code literary";
+ } else if (n < 82) {
+  return "code literature";
+ } else if (n < 84) {
+  return "code literati";
+ } else if (n < 86) {
+  return "code litterateurs";
+ } else if (n < 90) {
+  return "hack lit";
+ } else if (n == 90) {
+  return "hacker lit";
+ } else if (n == 91) {
+  return "hacked lit";
+ } else if (n == 92) {
+  return "coder lit";
+ } else if (n == 93) {
+  return "coded lit";
+ } else if (n == 94) {
+  return "phreak lit";
+ } else if (n == 95) {
+  return "superuser lit";
+ } else if (n == 96) {
+  return "cybernaut lit";
+ } else if (n == 97) {
+  return "silicon codex";
+ } else if (n == 98) {
+  return "code leet";
+ } else {
+  return "c0d3 1i7";
+ }
+}
+
+function type() {
+ var header = namer();
+ function typer() {
+  if (header == "") {
+   setTimeout(untype, 8192);
+   clearInterval(typer);
+  } else {
+   document.getElementById("header").innerHTML += header.slice(0, 1);
+   header = header.slice(1);
+  }
+ }
+ typer = setInterval(typer, 256);
+}
+
+function untype() {
+ function untyper() {
+  if (document.getElementById("header").innerHTML == "") {
+   setTimeout(type, 4096);
+   clearInterval(untyper);
+  } else {
+   document.getElementById("header").innerHTML = document.getElementById("header").innerHTML.slice(0, -1);
+  }
+ }
+ untyper = setInterval(untyper, 256);
+}
+
+setTimeout(type, 4096);
+
+function erase() {
+ clearInterval(rainbow);
+ none(document.getElementById("page"));
+ document.getElementById("egg").innerHTML = "";
+ opaque(document.body);
+ document.body.style.background = "#ffffff";
+ document.title = "\u00A0";
+}
+
+function read() {
+ var selection = document.getElementsByClassName("title")[random(document.getElementsByClassName("title").length)].innerHTML.toLowerCase();
+ var url = "";
+ var n = 0;
+ while (n < selection.length) {
+  if (selection[n] == " " && selection[n + 1] == "(") {
+   n = selection.length;
+  } else if (selection[n] == " ") {
+   url += "-";
+  } else if (selection[n] != "," && selection[n] != "." && selection[n] != ":" && selection[n] != ";" && selection[n] != "!" && selection[n] != "?" && selection[n] != "'") {
+   url += selection[n];
+  }
+  n++;
+ }
+ window.location = url;
+}
+
+var egg = "";
+
+var count = 0;
+
+document.addEventListener("keypress", function(event) {
+ if (event.code != "Space") {
+  egg += event.key.toLowerCase();
+ } else {
+  event.preventDefault();
+ }
+ if (egg.slice(-4) == "2600") {
+  opaque(document.body);
+  none(document.getElementById("egg"));
+  if (count == 0) {
+   document.getElementById("egg").innerHTML = '<audio controls preload="auto" id="2600"><source src="egg/2600-1.mp3" type="audio/mpeg"></audio>';
+   count++;
+  } else {
+   document.getElementById("egg").innerHTML = '<audio controls preload="auto" id="2600"><source src="egg/2600-2.mp3" type="audio/mpeg"></audio>';
+   count--;
+  }
+  document.getElementById("2600").play();
+  document.getElementById("2600").volume = 0.2;
+ }
+ if (egg.slice(-4) == "3301") {
+  opaque(document.body);
+  none(document.getElementById("egg"));
+  document.getElementById("egg").innerHTML = '<audio controls preload="auto" id="3301"><source src="egg/3301.mp3" type="audio/mpeg"></audio>';
+  document.getElementById("3301").play();
+  document.getElementById("3301").volume = 0.4;
+ }
+ if (egg.slice(-7) == "deadcow") {
+  opaque(document.body);
+  none(document.getElementById("egg"));
+  document.getElementById("egg").innerHTML = '<audio controls preload="auto" id="deadcow"><source src="egg/deadcow.mp3" type="audio/mpeg"></audio>';
+  document.getElementById("deadcow").play();
+  document.getElementById("deadcow").volume = 0.4;
+ }
+ if (egg.slice(-14) == "enterthematrix") {
+  opaque(document.body);
+  none(document.getElementById("egg"));
+  document.getElementById("egg").innerHTML = '<audio controls preload="auto" id="enterthematrix"><source src="egg/enterthematrix.mp3" type="audio/mpeg"></audio>';
+  document.getElementById("enterthematrix").play();
+  document.getElementById("enterthematrix").volume = 0.4;
+ }
+ if (egg.slice(-11) == "wearelegion") {
+  document.body.style.opacity = "0.9";
+  block(document.getElementById("egg"));
+  document.getElementById("egg").innerHTML = '<img class="large" src="egg/wearelegion.gif">';
+ }
+ if (egg.slice(-24) == "allyourbasearebelongtous") {
+  document.body.style.opacity = "0.9";
+  block(document.getElementById("egg"));
+  document.getElementById("egg").innerHTML = '<img class="small" src="egg/allyourbasearebelongtous.gif">';
+ }
+ if (egg.slice(-5) == "xyzzy") {
+  document.getElementById("egg").innerHTML = '<video preload="auto" height="140%" id="xyzzy"><source src="egg/xyzzy.mp4" type="video/mp4"></video>';
+  document.body.style.opacity = "0.9";
+  block(document.getElementById("egg"));
+  document.getElementById("xyzzy").play();
+  document.getElementById("xyzzy").volume = 0.2;
+ }
+ if (egg.length > 64) {
+  egg = egg.slice(-32);
+ }
+});
