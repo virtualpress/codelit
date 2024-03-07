bca592b1149c96f5f6a5a29abaf5bc72af867741
mwektaehtabr <mwektaehtabr@gmail.com>
2024-03-07 16:48:07 -0500

    //

--- in-memphis.php
+++ in-memphis.php
@@ -2,16 +2,31 @@
 <?php include "comment.php";?>
 <html dir="ltr" lang="en">
  <head>
-  <title>WRITER'S BLOCK // matthew baker</title>
+  <title>IN MEMPHIS // reed turchi</title>
   <?php include "meta.php";?>
-  <link rel="stylesheet" type="text/css" href="writers-block.css">
+  <link rel="stylesheet" type="text/css" href="in-memphis.css">
  </head>
  <body>
   <?php include "header.php";?>
-   <h2>WRITER'S BLOCK</h2>
-   <p class="byline">// <a href="https://mwektaehtabr.com/" class="author">matthew baker</a></p>
-   <div class="content"><p id="lit"></p></div>
+   <h2>IN MEMPHIS</h2>
+   <p class="byline">// <a href="https://www.reedturchi.com/" class="author">reed turchi</a></p>
+   <div class="content">
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+    <p class="lit"></p>
+   </div>
   <?php include "footer.php";?>
-  <script src="writers-block.js"></script>
+  <script src="in-memphis.js"></script>
  </body>
 </html>
